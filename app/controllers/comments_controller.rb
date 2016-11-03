0b9b7f411d2ce1aa465f7a9e201cce5f638b952f
KumanakaRyota <kumanaka-ryota@av.avex.co.jp>
2016-11-03 07:26:46 +0000

    merge前

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,6 +43,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :customer_id , :user_id)
+    params.require(:comment).permit(:body,:customer_id,:user_id)
   end
 end
